33c9321e5da24315c90aa838e5d389c3bd2d39ed
tejaswigowda <tejaswi@foxyninjastudios.com>
2016-09-21 13:13:41 -0700

    ac

--- sets/sets.js
+++ sets/sets.js
@@ -1,9 +1,25 @@
 var Sets = {
     union: function (setA, setB){
-        var setR;
-        
+        var setR = [];
+        var pointer = 0;
+        while(pointer < setA.length){
+          var currEle = setA[pointer]
+          if(setR.indexOf(currEle) < 0){
+              // currEle is not in setR
+            setR[setR.length] = currEle;
+          }
+          pointer++;
+        }        
 
-
+        pointer = 0;
+        while(pointer < setB.length){
+          var currEle = setB[pointer]
+          if(setR.indexOf(currEle) < 0){
+              // currEle is not in setR
+            setR[setR.length] = currEle;
+          }
+          pointer++;
+        }        
         return setR;
     }
 
